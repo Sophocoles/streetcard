973946d5ec89b7b56f67e4aed7d068825c61eebe
Sophocoles <mhowar19@asu.edu>
2023-06-16 14:54:37 -0700

    wagtasil 2x update

--- streetcard/home/models.py
+++ streetcard/home/models.py
@@ -9,9 +9,9 @@
 from django.db import models
 
 # Wagtail
-from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 ##################################################################################
 # Home Page
